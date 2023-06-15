cd7159bbfde879ce0e24766a60ff9b989d3d96f8
Satyajit Pradhan <PRADHANS@amdocs.com>
2023-06-15 13:38:15 +0530

    added addData method to TestingintegrationApplication.java file

--- src/main/java/com/example/TestingintegrationApplication.java
+++ src/main/java/com/example/TestingintegrationApplication.java
@@ -5,6 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestingintegrationApplication {
+	
+	public void addData(String data) {
+		
+		System.out.println("Thsi is a demo of git");
+		
+		if (data.equals("Java ")) {
+			System.out.println("This is inside if statement");
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingintegrationApplication.class, args);
